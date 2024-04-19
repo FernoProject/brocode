c24a044f2ec6ade1fb8f8e1a77f58d61e05dbf36
FernoProject <140708518+FernoProject@users.noreply.github.com>
2024-04-19 18:42:59 +0530

    Add files via upload

---
+++ practise_JavaScript.js
@@ -0,0 +1,13 @@
+/* var keyword - INITIALIZING THE DATA*/
+var a = 5;
+var b = 6;
+var c =  a+b;
+/* const keyword - INITIALIZING THE VARIABLE */
+const pi = 3.14;
+/* let keyword - INITIALIZING the variable */
+{
+    let d=10;
+    console.log(d);
+}
+console.log(pi);
+console.log(c);
